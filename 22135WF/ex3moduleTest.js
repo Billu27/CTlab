5ff355915b93382bd2ade3353c13dd67c6e05e83
Billu27 <153723738+Billu27@users.noreply.github.com>
2025-01-15 09:45:39 +0530

    wF LAB 3 - 15 JAN 25

---
+++ 22135WF/ex3moduleTest.js
@@ -0,0 +1,5 @@
+var m = require('./ex3_meth');
+
+console.log(m.add(3,5));
+console.log(m.multiply(4,5));
+console.log(m.factorial(5));
---
+++ 22135WF/ex3moduleTesxt.js
@@ -0,0 +1,8 @@
+var m = require('./ex3_meth');
+
+console.log(m.add(3,5));
+console.log(m.multiply(4,5));
+console.log(m.factorial(5));
+console.log(m.Date());
+
+//run this file in terminal using node ex3moduleTest.js
